6684f856acc761aac11007989c40fae2b70c0a81
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-07-10 10:40:03 +0200

    feat(Dinomaly): add StableAdamW optimizer and WarmCosineScheduler classes

--- src/anomalib/models/image/dinomaly/components/optimizer.py
+++ src/anomalib/models/image/dinomaly/components/optimizer.py
@@ -5,8 +5,6 @@ forked from https://github.com/pytorch/pytorch/blob/master/torch/optim/adamw.py
 import math
 import torch
 from torch.optim.optimizer import Optimizer
-import numpy as np
-from torch.optim.lr_scheduler import _LRScheduler
 
 
 class StableAdamW(Optimizer):
@@ -132,23 +130,3 @@ class StableAdamW(Optimizer):
                 p.data.addcdiv_(exp_avg, denom, value=-step_size)
 
         return loss
-
-
-class WarmCosineScheduler(_LRScheduler):
-
-    def __init__(self, optimizer, base_value, final_value, total_iters, warmup_iters=0, start_warmup_value=0, ):
-        self.final_value = final_value
-        self.total_iters = total_iters
-        warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
-
-        iters = np.arange(total_iters - warmup_iters)
-        schedule = final_value + 0.5 * (base_value - final_value) * (1 + np.cos(np.pi * iters / len(iters)))
-        self.schedule = np.concatenate((warmup_schedule, schedule))
-
-        super(WarmCosineScheduler, self).__init__(optimizer)
-
-    def get_lr(self):
-        if self.last_epoch >= self.total_iters:
-            return [self.final_value for base_lr in self.base_lrs]
-        else:
-            return [self.schedule[self.last_epoch] for base_lr in self.base_lrs]
