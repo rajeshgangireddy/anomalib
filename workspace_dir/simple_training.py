2c279e27c2a41743b43698e15ef3f6ffd26569a8
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-08-08 09:20:59 +0200

    Update simple_training.py

--- workspace_dir/simple_training.py
+++ workspace_dir/simple_training.py
@@ -1,8 +1,8 @@
+import torch
+
 from anomalib.data import MVTecAD
 from anomalib.engine import Engine, SingleXPUStrategy, XPUAccelerator
 from anomalib.models import Patchcore
-
-import torch
 
 cuda_available = torch.cuda.is_available()
 # Initialize components
@@ -11,13 +11,18 @@ datamodule = MVTecAD(category="toothbrush")
 model = Patchcore()
 if not cuda_available:
     # If CUDA is not available, use XPU strategy
+    print("CUDA is not available. Using XPU strategy.")
+    print(f"XPU Avialable: {torch.xpu.is_available()}")
+
     engine = Engine(
         strategy=SingleXPUStrategy(),
         accelerator=XPUAccelerator(),
         )
+
 else:
+    print("CUDA is available. Using default strategy.")
     # If CUDA is available, use the default strategy
     engine = Engine()
 # Train the model
 engine.fit(datamodule=datamodule, model=model)
-engine.test(datamodule=datamodule, model=model)
+results = engine.test(datamodule=datamodule, model=model)
