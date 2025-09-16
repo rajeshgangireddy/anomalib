461864eead81893ff4b1acddb577c414cd88e24c
rajeshgangireddy <rajesh.gangireddy@intel.com>
2025-09-16 22:07:46 +0200

    🚀 feat(device): update unsupported XPU list to include UHD Graphics 770

--- src/anomalib/utils/device.py
+++ src/anomalib/utils/device.py
@@ -6,7 +6,9 @@
 import torch
 
 # Set of xpu devices that might not support all funtions/operations out of the box
-UNSUPPORTED_XPU: frozenset = frozenset({"Intel(R) Iris(R) Xe Graphics"})
+# Update this set as we identify more unsupported devices
+# Raptor Lake can have two different iGPUs, Iris Xe or UHD 770 (desktop RPL-S)
+UNSUPPORTED_XPU: frozenset = frozenset({"Intel(R) Iris(R) Xe Graphics", "Intel(R) UHD Graphics 770"})
 
 
 def is_unsupported_xpu() -> bool:
