9861a7b07a64b8ea642f5f91cd6ccebe755ee325
GdoongMathew <gdoongmathew@gmail.com>
2026-01-06 21:52:51 +0800

    fix(docs): update model initialization arguments in get_model's docstring (#3234)
    * Update model initialization arguments in get_model's docstring
    
    Signed-off-by: GdoongMathew <gdoongmathew@gmail.com>
    
    * add back docs
    
    Signed-off-by: GdoongMathew <gdoongmathew@gmail.com>
    
    ---------
    
    Signed-off-by: GdoongMathew <gdoongmathew@gmail.com>

--- src/anomalib/models/__init__.py
+++ src/anomalib/models/__init__.py
@@ -255,25 +255,25 @@ def get_model(model: DictConfig | str | dict | Namespace, *args, **kwdargs) -> A
 
         >>> model = get_model("padim")
         >>> model = get_model("efficient_ad")
-        >>> model = get_model("patchcore", input_size=(100, 100))
+        >>> model = get_model("patchcore", num_neighbors=10)
 
         Get model using dictionary config:
 
         >>> model = get_model({"class_path": "Padim"})
         >>> model = get_model(
         ...     {"class_path": "Patchcore"},
-        ...     input_size=(100, 100)
+        ...     num_neighbors=10,
         ... )
         >>> model = get_model({
         ...     "class_path": "Padim",
-        ...     "init_args": {"input_size": (100, 100)}
+        ...     "init_args": {"backbone": "resnet18""}
         ... })
 
         Get model using fully qualified path:
 
         >>> model = get_model({
         ...     "class_path": "anomalib.models.Padim",
-        ...     "init_args": {"input_size": (100, 100)}
+        ...     "init_args": {"backbone": "resnet18""}
         ... })
     """
     model_: AnomalibModule
